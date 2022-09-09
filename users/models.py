be68dade2b1c42a39a31d456f9b47a00a9e174cc
WoeunJung <leena09123@naver.com>
2022-09-09 15:25:11 +0900

    fix: Profile max_length change

--- users/models.py
+++ users/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    nickname = models.CharField(max_length=10, default="")
+    nickname = models.CharField(max_length=50, default="")
     name = models.CharField(max_length=5, default="")
-    gender = models.BooleanField(default = True)
-    location = models.CharField(max_length=20, default="")
-    disabled = models.CharField(max_length=10, default="")
+    gender = models.BooleanField(default=True)
+    location = models.CharField(max_length=50, default="")
+    disabled = models.CharField(max_length=50, default="")
     profileImage = models.ImageField(upload_to='profile/', default='default.png')
-    introduction = models.CharField(max_length=150, default="")
+    introduction = models.CharField(max_length=300, default="")
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
