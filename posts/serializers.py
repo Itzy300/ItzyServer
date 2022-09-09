3b96c652c5eb75c586c69eea7b5820cfc7aff030
WoeunJung <leena09123@naver.com>
2022-09-09 20:38:44 +0900

    fix: 댓글 기능 구현(CRUD)

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 
 from users.serializers import ProfileGETSerializer
-from .models import Post
+from .models import Post,Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    profile = ProfileGETSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ("pk", "profile", "post", "text")
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("post", "text")
 
 
 class PostSerializer(serializers.ModelSerializer):
     profile = ProfileGETSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ("pk", "profile", "title", "body", "image", "published_date", "likes", "category")
+        fields = ("pk", "profile", "title", "body", "image",
+                  "published_date", "likes", "category", "comments")
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
